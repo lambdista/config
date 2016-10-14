7ee93e0b53efa9200f278b4b47e66e26b4d4fa44
Alessandro Lacava <alessandrolacava@gmail.com>
2016-10-14 10:57:03 +0200

    improve parser

--- core/src/main/scala/com/lambdista/config/ConfParser.scala
+++ core/src/main/scala/com/lambdista/config/ConfParser.scala
@@ -18,68 +18,77 @@ import com.lambdista.config.exception.ConfigSyntaxException
   */
 object ConfParser {
   case class NamedFunction[T, V](f: T => V, name: String) extends (T => V) {
-    def apply(t: T)         = f(t)
-    override def toString() = name
-
+    override def apply(t: T) = f(t)
+    override def toString()  = name
   }
 
   val Whitespace      = NamedFunction(" \r\n".contains(_: Char), "Whitespace")
   val Digits          = NamedFunction('0' to '9' contains (_: Char), "Digits")
   val StringChars     = NamedFunction(!"\"\\".contains(_: Char), "StringChars")
-  val IdentifierChars = NamedFunction(!":= ".contains(_: Char), "IdentifierChars")
+  val IdentifierChars = NamedFunction(!":=\" ".contains(_: Char), "IdentifierChars")
 
-  val optionalSpaces = P(CharsWhile(Whitespace).?)
-  val spaces         = P(CharsWhile(Whitespace))
-  val digits         = P(CharsWhile(Digits))
-  val exponent       = P(CharIn("eE") ~ CharIn("+-").? ~ digits)
-  val fractional     = P("." ~ digits)
-  val integral       = P("0" | CharIn('1' to '9') ~ digits.?)
+  val optionalSpaces: Parser[Unit] = P(CharsWhile(Whitespace).?)
+  val spaces: Parser[Unit]         = P(CharsWhile(Whitespace))
 
-  val number: Parser[Double] = P(CharIn("+-").? ~ integral ~ fractional.? ~ exponent.?).!.map(_.toDouble)
-  val abstractNumber         = number.map(AbstractNumber)
+  val singlelineComment: Parser[Unit] = P("//" ~/ AnyChar.rep ~ spaces)
+
+  val digits: Parser[Unit]                   = P(CharsWhile(Digits))
+  val exponent: Parser[Unit]                 = P(CharIn("eE") ~ CharIn("+-").? ~ digits)
+  val fractional: Parser[Unit]               = P("." ~ digits)
+  val integral: Parser[Unit]                 = P("0" | CharIn('1' to '9') ~ digits.?)
+  val number: Parser[Double]                 = P(CharIn("+-").? ~ integral ~ fractional.? ~ exponent.?).!.map(_.toDouble)
+  val abstractNumber: Parser[AbstractNumber] = number.map(AbstractNumber)
 
   val `null`: Parser[AbstractNone.type] = P("null").map(_ => AbstractNone)
-  val `false`: Parser[AbstractBool]     = P("false").map(_ => AbstractBool(false))
-  val `true`: Parser[AbstractBool]      = P("true").map(_ => AbstractBool(true))
 
-  val hexDigit      = P(CharIn('0' to '9', 'a' to 'f', 'A' to 'F'))
-  val unicodeEscape = P("u" ~ hexDigit ~ hexDigit ~ hexDigit ~ hexDigit)
-  val escape        = P("\\" ~ (CharIn("\"/\\bfnrt") | unicodeEscape))
+  val `false`: Parser[AbstractBool] = P("false").map(_ => AbstractBool(false))
+  val `true`: Parser[AbstractBool]  = P("true").map(_ => AbstractBool(true))
 
-  val strChars = P(CharsWhile(StringChars))
-  val idChars  = P(CharsWhile(IdentifierChars))
+  val hexDigit: Parser[Unit]      = P(CharIn('0' to '9', 'a' to 'f', 'A' to 'F'))
+  val unicodeEscape: Parser[Unit] = P("u" ~ hexDigit ~ hexDigit ~ hexDigit ~ hexDigit)
+  val escape: Parser[Unit]        = P("\\" ~ (CharIn("\"/\\bfnrt") | unicodeEscape))
+
+  val strChars: Parser[Unit] = P(CharsWhile(StringChars))
+  val idChars: Parser[Unit]  = P(CharsWhile(IdentifierChars))
 
   val string: Parser[String] = P(optionalSpaces ~ "\"" ~/ (strChars | escape).rep.! ~ "\"")
 
   val abstractString: Parser[AbstractString] = string.map(AbstractString)
 
-  val freeIdentifier: Parser[String] = P(optionalSpaces ~/ (idChars | escape).rep.!)
+  val unquotedIdentifier: Parser[String] = P((idChars | escape).rep.!)
+  val quotedIdentifier: Parser[String]   = "\"" ~/ unquotedIdentifier ~ "\""
 
-  val identifier: Parser[String] = string | freeIdentifier
+  val identifier: Parser[String] = P(optionalSpaces ~/ (quotedIdentifier | unquotedIdentifier))
 
   val array: Parser[AbstractList] =
     P("[" ~/ jsonExpr.rep(sep = ",".~/) ~ optionalSpaces ~ "]").map(xs => AbstractList(xs.toList))
 
-  val pair = P(identifier ~/ optionalSpaces ~ (":" | "=") ~/ jsonExpr)
+  val pair: Parser[(String, AbstractValue)] = P(identifier ~/ optionalSpaces ~ (":" | "=") ~/ jsonExpr)
 
   val obj: Parser[AbstractMap] =
     P("{" ~/ pair.rep(sep = ",".~/) ~ optionalSpaces ~ "}").map(x => AbstractMap(x.toMap))
 
   object DurationDecoder {
-    def apply(value: Double, unit: String): Duration = {
-      val timeUnit: TimeUnit = unit match {
-        case "days" | "day" | "d"                                       => TimeUnit.DAYS
-        case "hours" | "hour" | "h"                                     => TimeUnit.HOURS
-        case "minutes" | "minute" | "min"                               => TimeUnit.MINUTES
-        case "seconds" | "second" | "secs" | "sec" | "s"                => TimeUnit.SECONDS
-        case "milliseconds" | "millisecond" | "millis" | "milli" | "ms" => TimeUnit.MILLISECONDS
-        case "microseconds" | "microsecond" | "micros" | "µs"           => TimeUnit.MICROSECONDS
-        case "nanoseconds" | "nanosecond" | "nanos" | "nano" | "ns"     => TimeUnit.NANOSECONDS
-        //      case "Inf" | "Inf" => TimeUnit
-        case _ => throw new ConfigSyntaxException("Could not recognize time unit in duration")
+    def apply(value: Option[Double], unit: String): Duration = {
+      def buildFinite(v: Double): Duration = {
+        val timeUnit: TimeUnit = unit match {
+          case "days" | "day" | "d"                                       => TimeUnit.DAYS
+          case "hours" | "hour" | "h"                                     => TimeUnit.HOURS
+          case "minutes" | "minute" | "min"                               => TimeUnit.MINUTES
+          case "seconds" | "second" | "secs" | "sec" | "s"                => TimeUnit.SECONDS
+          case "milliseconds" | "millisecond" | "millis" | "milli" | "ms" => TimeUnit.MILLISECONDS
+          case "microseconds" | "microsecond" | "micros" | "µs"           => TimeUnit.MICROSECONDS
+          case "nanoseconds" | "nanosecond" | "nanos" | "nano" | "ns"     => TimeUnit.NANOSECONDS
+        }
+        Duration(v, timeUnit)
       }
 
-      Duration(value, timeUnit)
+      def buildInfinite(): Duration = unit match {
+        case "Inf"      => Duration.Inf
+        case "MinusInf" => Duration.MinusInf
+      }
+
+      value.map(buildFinite).getOrElse(buildInfinite())
     }
   }
   val validDuration = "days" | "day" | "d" |
@@ -88,16 +97,35 @@ object ConfParser {
       "seconds" | "second" | "secs" | "sec" | "s" |
       "milliseconds" | "millisecond" | "millis" | "milli" | "ms" |
       "microseconds" | "microsecond" | "micros" | "µs" |
-      "nanoseconds" | "nanosecond" | "nanos" | "nano" | "ns"
-
-  val duration: Parser[Duration] = P(number.! ~ spaces ~ validDuration.!).map {
+      "nanoseconds" | "nanosecond" | "nanos" | "nano" | "ns" |
+      "Inf" | "MinusInf"
+  val duration: Parser[Duration] = P((number.! ~ spaces).? ~ validDuration.!).map {
     case (value, unit) =>
-      DurationDecoder(value.toDouble, unit)
+      DurationDecoder(value.map(_.toDouble), unit)
   }
   val abstractDuration: Parser[AbstractDuration] = duration.map(AbstractDuration)
 
+  def createRange(start: Int, method: String, end: Int, optStep: Option[Int]): Range = {
+    val baseRange = method match {
+      case "to"    => start to end
+      case "until" => start until end
+    }
+
+    optStep.map(step => baseRange by step).getOrElse(baseRange)
+  }
+  val intRange: Parser[Range] =
+    P(integral.! ~ spaces ~ ("to" | "until").! ~ spaces ~ integral.! ~ (spaces ~ "by" ~ spaces ~ integral.!).? ~ optionalSpaces).map {
+      case (start, method, end, optStep) => createRange(start.toInt, method, end.toInt, optStep.map(_.toInt))
+    }
+  val charRange: Parser[Range] =
+    P("'" ~ AnyChar.! ~ "'" ~ spaces ~ ("to" | "until").! ~ spaces ~ "'" ~ AnyChar.! ~ "'" ~ (spaces ~ "by" ~ spaces ~ integral.!).? ~ optionalSpaces).map {
+      case (start, method, end, optStep) => createRange(start.head.toInt, method, end.head.toInt, optStep.map(_.toInt))
+    }
+  val range: Parser[Range]                 = intRange | charRange
+  val abstractRange: Parser[AbstractRange] = range.map(AbstractRange)
+
   val jsonExpr: Parser[AbstractValue] = P(
-    optionalSpaces ~ (obj | array | abstractDuration | abstractString | `true` | `false` | `null` | abstractNumber) ~ optionalSpaces
+    optionalSpaces ~ (obj | array | abstractDuration | abstractRange | abstractString | `true` | `false` | `null` | abstractNumber) ~ optionalSpaces
   )
 
   def parse(confStr: String): Try[Config] = {
@@ -113,11 +141,25 @@ object ConfParser {
 }
 
 object ConfParserTest {
-  case class Conf(omg: String, wtf: Double, duration: Duration)
+  case class Conf(
+      omg: String,
+      wtf: Double,
+      infinite: Duration,
+      finite: Duration,
+      charRange: List[Char],
+      intRange: Range
+  )
 
   def main(args: Array[String]): Unit = {
     val result: Try[Config] = ConfParser.parse(
-      """{omg   = "123", "wtf": 12.4123, duration = 5 seconds}"""
+      """{
+          |omg   = "123",
+          |"wtf": 12.4123,
+          |infinite = Inf,
+          |finite = 5 millis,
+          |intRange: 0 to 4 by 2,
+          |charRange: 'a' to 'c'
+        |}""".stripMargin
     )
 
     val conf = result.flatMap(_.as[Conf])

7c6bf19a1a3cd435e9add9fcf502228d01da4243
Alessandro Lacava <alessandrolacava@gmail.com>
2019-06-13 11:55:45 +0200

    add condition to cross publish given fastparse 2.1.3 is not published for scala 2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,19 +8,22 @@ object Dependencies {
   lazy val shapelessVersion      = "2.3.3"
   lazy val typesafeConfigVersion = "1.3.4"
   lazy val fastparseVersion      = "2.1.3"
+  lazy val fastparseVersion2_11  = "2.1.2"
 
   // Libraries
   val scalatest      = "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
   val shapeless      = "com.chuusai"   %% "shapeless" % shapelessVersion
   val typesafeConfig = "com.typesafe"  % "config"     % typesafeConfigVersion
   val fastparse      = "com.lihaoyi"   %% "fastparse" % fastparseVersion
+  val fastparse2_11  = "com.lihaoyi"   %% "fastparse" % fastparseVersion2_11
 
   // Projects
-  val coreDeps = Seq(
+  lazy val coreCommonDeps = Seq(
     scalatest,
-    shapeless,
-    fastparse
+    shapeless
   )
+  lazy val coreDeps     = coreCommonDeps :+ fastparse
+  lazy val coreDeps2_11 = coreCommonDeps :+ fastparse2_11
 
-  val typesafeDeps = Seq(typesafeConfig, scalatest)
+  lazy val typesafeDeps = Seq(typesafeConfig, scalatest)
 }

1083d89d280bebd4ddcf04fb405544a6109c7c9b
lambdista <alessandrolacava@gmail.com>
2020-05-06 14:49:49 +0200

    switch from Try to Either to handle possible failures

--- core/src/main/scala/com/lambdista/config/ConfigWalker.scala
+++ core/src/main/scala/com/lambdista/config/ConfigWalker.scala
@@ -1,9 +1,6 @@
 package com.lambdista.config
 
 import scala.language.dynamics
-import scala.util.{Failure, Try}
-
-import com.lambdista.util.syntax.std.option._
 
 /**
   * This class lets you walk across a configuration dynamically. Nonetheless it manages possible errors within
@@ -12,12 +9,12 @@ import com.lambdista.util.syntax.std.option._
   * @author Alessandro Lacava (@lambdista)
   * @since 2016-11-23
   */
-final case class ConfigWalker(value: Try[AbstractValue]) extends Dynamic {
+final case class ConfigWalker(value: Result[AbstractValue]) extends Dynamic {
   def selectDynamic(key: String): ConfigWalker = {
     val searchResult = value.flatMap {
       case x: AbstractMap => x.get(key)
       case x =>
-        Failure(
+        Left(
           new KeyNotFoundError(s"$x is not an AbstractMap so the $key key does not make sense on this object")
         )
     }
@@ -25,9 +22,9 @@ final case class ConfigWalker(value: Try[AbstractValue]) extends Dynamic {
     ConfigWalker(searchResult)
   }
 
-  def as[A: ConcreteValue]: Try[A] =
+  def as[A: ConcreteValue]: Result[A] =
     for {
       abstractValue <- value
-      concreteValue <- ConcreteValue[A].apply(abstractValue).toTry(new ConversionError(abstractValue))
+      concreteValue <- ConcreteValue[A].apply(abstractValue).toRight(new ConversionError(abstractValue))
     } yield concreteValue
 }
